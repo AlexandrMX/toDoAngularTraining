e9b2a248feedb67ca140a00bfd2e5b9ffe17b44b
unknown <sanjaruv@inbox.lv>
2019-07-16 22:44:56 +0200

    form, create, delete task

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,52 @@
 import { Component } from '@angular/core';
 
+class Todo {
+  constructor(public title: string,
+              public completed: boolean = false) {}
+}
+
+const todos: Todo[] = [
+  {
+    title: "learn JS",
+    completed: true
+  },
+  {
+    title: "learn Angular",
+    completed: false
+  },
+  {
+    title: "learn TS",
+    completed: false
+  }
+];
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'to-do-training';
+  title: string = 'to-do-training';
+  todos: Todo[] = todos;
+  newTodoTitle: string = "";
+
+  create() {
+    let todo: Todo = new Todo(this.newTodoTitle);
+    this.todos.push(todo);
+    this.newTodoTitle = '';
+  }
+
+  toggle(todo: Todo) {
+    todo.completed = !todo.completed;
+
+  }
+
+  delete(todo: Todo) {
+    let index = this.todos.indexOf(todo);
+
+    if (index > -1) {
+      this.todos.splice(index, 1);
+    }
+
+  }
 }
